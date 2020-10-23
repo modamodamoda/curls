5289339033dead1d141fcf816f224d597d223a91
Jack Penny <me@jackpenny.co.uk>
2020-10-23 12:25:56 +0200

    Fixes, updates

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const lexer = require('./lib/lexer');
     allows you to feed variables to it.
 */
 
-class TranspileMachine { // Registry for defined variables and code blocks
+class TranspileMachine { // Transpiles token opcodes into code blocks
     blocks = {};
     blockStack = [];
-    curBlock = null;
     constructor() {
         this.block('root'); // start root code block
     }
@@ -26,7 +25,7 @@ class TranspileMachine { // Registry for defined variables and code blocks
             if(block == 'root') rootBlock = this.blocks[block];
             else blocks.push(this.blocks[block]);
         }
-        return blocks.map((b) => `let block_${b.id} = () => {${b.data}}`).join('\n') + '\n' + rootBlock.data;
+        return blocks.map((b) => `let block_${b.id} = () => {let ret = ''\n${b.data}\nreturn ret}`).join('\n') + '\nlet ret=\'\'\n' + rootBlock.data + '\nreturn ret';
     }
     block(id = null) {
         // begins a block
@@ -35,13 +34,11 @@ class TranspileMachine { // Registry for defined variables and code blocks
         }
         this.blocks[id] = {id: id, data: ''};
         this.blockStack.push(this.blocks[id]);
-        this.curBlock = this.blocks[id];
         return 'block_' + id;
     }
     endBlock() {
         // ends a block
         this.blockStack.pop();
-        this.curBlock = this.blockStack[this.blockStack.length - 1];
     }
     write(code, depth = 1) {
         // writes code to a block, capable of going up several blocks
@@ -74,11 +71,18 @@ class TranspileMachine { // Registry for defined variables and code blocks
         }
         return ret;
     }
+    // Wrap in return JS
+    ret(str, raw) {
+        return 'ret += ' + (raw ? str : 'this.__entities(' + str + ')');
+    }
     // Transpile blocks into JS
+    // Blocks are in form of: [lexPos, tokens, grammar]
+    // lexPos is used by the parser and not here, tokens is the opcodes/tokens, and grammar is the name of the grammar matched
     transpile(blocks, nested = false) {
         let exprStack = [];
         let ret ='';
         const writeBuf = (depth = 1) => {
+            if(nested) throw 'Block expressions shouldn\'t exist in nested operations, the parser must have screwed up...';
             this.write(ret, depth);
             ret = '';
         }
@@ -86,14 +90,14 @@ class TranspileMachine { // Registry for defined variables and code blocks
             let opts = blockHead.opts || {};
             let block = blockHead.block;
 
-            let printFunc = opts.raw ? '__printRaw' : '__print';
-
-            if(opts.html) {
-                ret += 'this.__printHTML(' + blockHead.data + ')\n';
+            if(opts.html) { // this is just HTML
+                ret += 'ret += this.__html(' + blockHead.data + ')\n';
                 continue;
             }
+
             let opcodes = block[1];
             let i, argList;
+
             switch(block[2]) {
                 case 'FUNC':
                     argList = [this.varname(opcodes[0][1])];
@@ -105,68 +109,68 @@ class TranspileMachine { // Registry for defined variables and code blocks
                     if(nested)
                         ret +=('this.__ev(' + argList.join(',') + ')');
                     else
-                        ret +=('this.' + printFunc + '(this.__ev(' + argList.join(',') + '))');
+                        ret += this.ret('this.__ev(' + argList.join(',') + ')', opts.raw);
                     break;
                 case 'VAR':
                     if(nested)
-                        ret +=(this.translate(opcodes));
+                        ret += this.translate(opcodes);
                     else
-                        ret +=('this.' + printFunc + '(' + this.translate(opcodes) + ')');
+                        ret += this.ret(this.translate(opcodes), opts.raw);
                     break;
                 case 'EACH':
                     exprStack.push('each');
-                    ret +=(`this.__each(${this.translate([opcodes[0]])}, "${opcodes[1][0] == 'JSV' ? opcodes[1][1] : 'this'}", ${this.block()})`);
+                    ret += this.ret(`this.__each(${this.translate([opcodes[0]])}, "${opcodes[1][0] == 'JSV' ? opcodes[1][1] : 'this'}", ${this.block()})`, opts.raw);
                     writeBuf(2);
                     break;
                 case 'WITH':
                     exprStack.push('with');
-                    ret +=(`this.__with(${this.translate([opcodes[0]])}, ${this.block()})`);
+                    ret += this.ret(`this.__with(${this.translate([opcodes[0]])}, ${this.block()})`, opts.raw);
                     writeBuf(2);
                     break;
-                case 'ENDEXPR':
+                case 'ENDBLOCK':
                     let a = exprStack.pop();
-                    if(a != opcodes[0][1]) throw 'Unexpected end expression!';
+                    if(a != opcodes[0][1]) throw 'Unexpected end block!';
                     if(opcodes[0][1] != 'if') {
                         writeBuf(1);
                         this.endBlock();
                     } else {
-                        ret +=( '}');
+                        ret += '}';
                     }
                     break;
                 case 'IF':
                     exprStack.push('if');
-                    ret +=('if(');
+                    ret += 'if(';
                     i = 0;
                     while(opcodes[i][0] != 'END') {
                         if(opcodes[i][0] == 'ARG') {
-                            ret +=(this.translate(opcodes[i][1]));
+                            ret += this.translate(opcodes[i][1]);
                         } else {
-                            ret +=(opcodes[i][1]);
+                            ret += opcodes[i][1];
                         }
                         i++;
                     }
-                    ret +=('){');
+                    ret += '){';
                     break;
                 case 'ELSE':
                     if(exprStack[exprStack.length - 1] == 'if' || exprStack[exprStack.length - 1] == 'unless')
-                        ret +=('} else {');
+                        ret += '} else {';
                     else
-                        ret +=('},() => {');
+                        ret += '},() => {';
                     break;
                 case 'ELIF':
-                    ret +=('} else if(');
+                    ret += '} else if(';
                     i = 0;
                     while(opcodes[i][0] != 'END') {
                         if(opcodes[i][0] == 'ARG') {
-                            ret +=(this.translate(opcodes[i][1]));
+                            ret += this.translate(opcodes[i][1]);
                         } else {
-                            ret +=(opcodes[i][1]);
+                            ret += opcodes[i][1];
                         }
                         i++;
                     }
                     ret +=('){');
                     break;
-                case 'EXPR':
+                case 'BLOCK':
                     exprStack.push(opcodes[0][1]);
                     argList = [];
                     i = 1;
@@ -174,15 +178,15 @@ class TranspileMachine { // Registry for defined variables and code blocks
                         argList.push(this.translate([opcodes[i]]));
                         i++;
                     }
-                    ret +=(`this.__runExpr('${opcodes[0][1]}',[${argList.join(',')}], ${this.block()});`);
+                    ret += this.ret(`this.__runBlock('${opcodes[0][1]}',[${argList.join(',')}], ${this.block()})`, opts.raw);
                     writeBuf(2);
                     break;
                 case 'PARTIAL':
-                    ret +=(`this.__runPart('${opcodes[0][1]}', ${this.translate([opcodes[1]])})`);
+                    ret += this.ret(`this.__runPart('${opcodes[0][1]}', ${this.translate([opcodes[1]])})`, opts.raw);
                     break;
-                case 'EXPRPART':
+                case 'BLOCKPART':
                     exprStack.push(opcodes[0][1]);
-                    ret +=(`this.__runPart('${opcodes[0][1]}', ${this.block()})`);
+                    ret += this.ret(`this.__runPart('${opcodes[0][1]}', ${this.block()})`, opts.raw);
                     writeBuf(2);
                     break;
                 case 'CREATEPART':
@@ -194,24 +198,24 @@ class TranspileMachine { // Registry for defined variables and code blocks
                     break;
                 case 'OBJ':
                     i = 0;
-                    ret +=('{');
+                    ret += '{';
                     while(opcodes[i][0] != 'END') {
                         if(opcodes[i][0] == 'JSV') {
-                            ret +=((i > 0 ? ',' : '') + opcodes[i][1]);
+                            ret += (i > 0 ? ',' : '') + opcodes[i][1];
                         } else {
-                            ret +=(this.translate([opcodes[i]]));
+                            ret += this.translate([opcodes[i]]);
                         }
                         i++;
                     }
-                    ret +=('}');
+                    ret += '}';
                     break;
                 default:
-                    ret +=(this.translate(opcodes));
+                    ret += this.translate(opcodes);
                     break;
             }
             ret += !nested ? '\n' : '';
         }
-        if(exprStack.length > 0) throw 'Unterminated expression!';
+        if(exprStack.length > 0) throw 'Unterminated block!';
         if(nested) return ret;
         else this.write(ret);
     }
@@ -220,10 +224,10 @@ class TranspileMachine { // Registry for defined variables and code blocks
 
 module.exports = class {
     vars = {} // Global variables
-    expr = {} // Expressions
+    blk = {} // Blocks
     part = {} // Partials
     constructor() {
-        this.block('where', (block, print, context, n, obj) => {
+        this.block('where', (block, context, n, obj) => {
             if(!obj || typeof obj != 'object') throw 'Invalid object passed to where function';
             if(obj._by) {                        
                 n = n.sort((a, b) => {
@@ -236,6 +240,7 @@ module.exports = class {
             }
             delete obj._by;
             delete obj._order; // don't need these anymore
+            let ret = '';
             n.forEach((a) => {
                 let unmatched = false;
                 for(let i in obj) {
@@ -244,15 +249,16 @@ module.exports = class {
                         break;
                     }
                 }
-                if(!unmatched) block(a);
+                if(!unmatched) ret += block(a);
             });
+            return ret;
         });
     }
     set(k, v) { // Set a global
         this.vars[k] = v;
     }
     block(k, v) {
-        this.expr[k] = v;
+        this.blk[k] = v;
     }
     partial(k, v) {
         this.part[k] = this.transpile(v);
@@ -262,7 +268,6 @@ module.exports = class {
         let render = [];
         let html = [];
         
-        let tknList = {};
         let lpadNext = false, lpSet = false;
         for(let i in blocks) {
             if(blocks[i][0][0] == 'WSP') {
@@ -278,9 +283,9 @@ module.exports = class {
                 render.push({ opts: { html: 1 }, data: (html.push( lpadNext ? blocks[i][0][1].replace(/^[\s]+/g, '') : blocks[i][0][1] ) - 1)});
             } else if(blocks[i][0][0] == 'RAWHTML') {
                 blocks[i].shift();
-                render.push({ block: parse(str, blocks[i], 0, null, tknList), opts: {raw: 1} });
+                render.push({ block: parse(str, blocks[i], 0, null), opts: {raw: 1} });
             } else {
-                render.push({ block: parse(str, blocks[i], 0, null, tknList) });
+                render.push({ block: parse(str, blocks[i], 0, null) });
             }
             if(!lpSet) lpadNext = false;
             else lpSet = false;
@@ -294,7 +299,6 @@ module.exports = class {
         return (function(args) { // return a JS function which contains the transpiled code
             let localVars = { __vars: Object.assign({}, args) };
             let scope = []; // block expression locally scoped vars
-            let buf = '';
             let getScope = (depth = 0) => {
                 if(scope.length > 0) {
                     if(scope.length - depth < 0) throw "Scope non-existent";
@@ -303,6 +307,7 @@ module.exports = class {
                 else return localVars.__vars;
             }
             localVars.__ev = (i, ...as) => { // resolve/eval using hashtable
+                // todo: cache already-resolved ptrs into a hashtable, the issue is automatically removing scoped ones
                 let scopeDepth = 0, g = 0;
                 if(i[0] == '..') { // a scoped variable
                     while(i[g] == '..') {
@@ -346,44 +351,56 @@ module.exports = class {
             localVars.__printRaw = (str) => {
                 buf += str;
             } 
-            localVars.__printHTML = (n) => {
-                buf += html[n];
+            localVars.__entities = (str) => { // todo: add unicode support
+                if(typeof str !== 'string') return str;
+                else return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+            }
+            localVars.__html = (n) => {
+                return html[n];
             }
             localVars.__each = (v, a, f) => {
                 let old = null;
                 let curScope = getScope();
                 if(curScope[a] !== undefined) old = curScope[a];
+                let ret = '';
                 v.forEach((el) => {
                     curScope[a] = el;
-                    f();
+                    ret += f();
                 });
                 if(old !== null) curScope[a] = old;
                 else delete curScope[a];
+                return ret;
             }
             localVars.__with = (v, f) => {
+                let ret = '';
                 v.forEach((el) => {
                     scope.push(el);
-                    f();
+                    ret += f();
                     scope.pop();
                 });
+                return ret;
             }
-            localVars.__expressions = _this.expr;
-            localVars.__runExpr = (n, a, f) => {
-                localVars.__expressions[n]((obj) => {
-                    if(obj === undefined) f();
+            localVars.__blocks = _this.blk;
+            localVars.__runBlock = (n, a, f) => {
+                let ret = '';
+                ret += localVars.__blocks[n]((obj) => {
+                    let ret = '';
+                    if(obj === undefined) ret += f();
                     else {
                         scope.push(obj);
-                        f();
+                        ret += f();
                         scope.pop();
                     }
-                 }, localVars.__print, getScope(), ...a);
-                
+                    return ret;
+                 }, getScope(), ...a);
+                return ret;
             }
             localVars.__runPart = (n, a) => {
                 // run a partial
                 let curScope = getScope();
                 let partial;
                 let local = true;
+                let ret = '';
                 if(!curScope.__partials || !curScope.__partials[n]) {
                     partial = _this.part[n];
                     local = false;
@@ -394,22 +411,23 @@ module.exports = class {
                     // block partial, this passes other partials to a partial
                     scope.push({});
                     a(); // this function likely registers some partials
-                    if(local) partial();
-                    else localVars.__printRaw(partial(getScope())); // run the partial in context
+                    if(local) ret = partial();
+                    else ret = partial(getScope()); // run the partial in context
                     scope.pop(); // clean off the scope
                 } else {
                     scope.push(a);
-                    if(local) partial();
-                    else localVars.__printRaw(partial(a));
+                    if(local) ret = partial();
+                    else ret = partial(a);
                     scope.pop();
                 }
+                return ret;
             }
             localVars.__createPart = (n, f) => {
                 let curScope = getScope();
                 if(!curScope.__partials) curScope.__partials = {};
                 curScope.__partials[n] = f;
             }
-            new Function(`
+            let buf = new Function(`
                 let __html = ${JSON.stringify(html)};
                 ${blocks_transpiled}
             `).call(localVars);
